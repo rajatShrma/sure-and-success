c4003ada9b4184de998a4cef3deb58216220e994
micro-ice <rajatshrma27@gmail.com>
2024-09-11 17:30:36 +0545

    UI changes made in website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,13 +34,13 @@ $(document).ready(function(){
 	// 2. welcome animation support
 
         $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-txt button").removeClass("animated fadeInDown").css({'opacity':'0'});
+        	$(".main-content-txt h2,.main-content-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".main-content-txt button").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
         $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-txt button").addClass("animated fadeInDown").css({'opacity':'0'});
+        	$(".main-content-txt h2,.main-content-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".main-content-txt button").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
 	
